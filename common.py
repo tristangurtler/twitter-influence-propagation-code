2f5db8d772a0e7766dfe1d995219982935c4a5b0
tristangurtler <tgurtler@illinois.edu>
2016-06-08 18:20:34 -0400

    changed for usage with OAuth2 instead of OAuth1.0

--- common.py
+++ common.py
@@ -9,13 +9,13 @@ ACCESS_TOKEN = 'Access token (see below) goes here>'
 
 # In order to get an access token, use the following code
 
-APP_SECRET = '<App Secret ("Consumer Secret") goes here>'
+# APP_SECRET = '<App Secret ("Consumer Secret") goes here>'
 
-twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
-ACCESS_TOKEN = twitter.obtain_access_token()
+# twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
+# ACCESS_TOKEN = twitter.obtain_access_token()
 
-with open("token.txt", 'wb') as output:
-    output.write(ACCESS_TOKEN + "\n")
+# with open("token.txt", 'wb') as output:
+#     output.write(ACCESS_TOKEN + "\n")
 
 # You will find the access token written in a file "token.txt"
 # in the folder you run this code in
