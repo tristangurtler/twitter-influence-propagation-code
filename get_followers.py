1897ffd4fab907db68fdc4bcc876c80c81ea68a2
tristangurtler <tgurtler@illinois.edu>
2016-06-09 15:37:19 -0400

    small changes, mostly moving code to common.py

--- get_followers.py
+++ get_followers.py
@@ -7,97 +7,12 @@
 #############################################################
 
 import argparse
-import sys
-import time
 import numpy as np
-
-from common import *
-from datetime import datetime
-from tqdm import tqdm
-from twython import Twython
-
-from twython.exceptions import TwythonError
-from twython.exceptions import TwythonAuthError
-from twython.exceptions import TwythonRateLimitError
-
-
-twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
-
-def get_timeline(screen_name):
-    global twitter
-    tweets = []
-
-    # We need at least one tweet to start with
-    while True:
-        try:
-            user_timeline = twitter.get_user_timeline(screen_name=screen_name, count=1)
-            break
-        except TwythonAuthError as e:
-            print "TwythonAuthError..."
-            time.sleep(1)
-            continue
-        except TwythonRateLimitError as e:
-            print "TwythonRateLimitError... Waiting for 15 minutes"
-            for timer in tqdm(range(15*60)):
-                time.sleep(1)
-            twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
-            time.sleep(1)
-            continue
-    
-    # put that one tweet into our list
-    tweets.extend(user_timeline)
-    tid = user_timeline[0]['id']
-    
-    # get more tweets! (up to 1500 more)
-    print "Getting tweets of " + screen_name + "..."
-    for i in tqdm(range(1,16)):
-        while True:
-            try:
-                time.sleep(1)
-                # this works because Twython gives us tweets in reverse chronological order (don't want to repeat, so set a max)
-                user_timeline = twitter.get_user_timeline(screen_name=screen_name, count=100, max_id=tid, include_retweets=False)
-                break
-            except TwythonAuthError as e:
-                print "TwythonAuthError..."
-                continue
-            except TwythonRateLimitError as e:
-                print "TwythonRateLimitError... Waiting for 15 minutes"
-                for timer in tqdm(range(15*60)):
-                    time.sleep(1)
-                twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
-                continue
-
-        tweets.extend(user_timeline)
-        tid = user_timeline[-1]['id']
-    
-    return tweets
-
-
-def get_timeline_in_range(screen_name, start_date, end_date):
-    start_date = datetime.strptime(start_date, "%Y-%m-%d")
-    end_date = datetime.strptime(end_date, "%Y-%m-%d")
-    result = []
-
-    # pull the (up to) 1501 most recent tweets put out by user "<screen_name>"
-    user_timeline = get_timeline(screen_name)
-
-    # select out only those tweets which fall within the appropriate timeframe
-    # (NOTE: this code assumes that the timeframe will be a subset of the 1401 most recent tweets,
-    # which for recent events and/or typical usage, seems fairly reasonable)
-    for tweet in user_timeline:
-        dt = datetime.strptime(tweet['created_at'], "%a %b %d %H:%M:%S +0000 %Y")
-        if dt >= start_date and dt <= end_date:
-            result.append(tweet)
-
-    # let our program's user know how many tweets fit in the time frame
-    print "Found ", len(result), " tweets in date range ", start_date, " to ", end_date
-    return result
-
+import common
 
 def main(args):
-    global twitter
     #Get tweets for the user in the date range
-    user_timeline = get_timeline_in_range(args.handle, args.start_date, args.end_date)
+    user_timeline = common.get_timeline_in_range(args.handle, args.start_date, args.end_date, 1)
 
     ################################################################################################################
     # 30 tweets to check is a magic number!!! Should it scale with the number of followers to pull?                #
@@ -117,70 +32,24 @@ def main(args):
     #select 30 tweets randomly in this range
     if len(user_timeline) > 30:
         user_timeline = np.random.choice(user_timeline, 30, replace=False)
-    retweeter_ids = []
-
-    #for each tweet, collect the retweeters
-    print "Getting retweeters of " + args.handle + "..."
-    for tweet in tqdm(user_timeline):
-        while True:
-            try:
-                time.sleep(1)
-                result = twitter.get_retweeters_ids(id=tweet['id'], count=50)
-                retweeter_ids.extend(result['ids'])
-                break
-            except TwythonAuthError as e:
-                print "TwythonAuthError..."
-                continue
-            except TwythonRateLimitError as e:
-                print "TwythonRateLimitError... Waiting for 15 minutes"
-                for timer in tqdm(range(15*60)):
-                    time.sleep(1)
-                twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
-                continue
-
-    #shuffle the retweeters to select retweeters (active followers) randomly
-    np.random.shuffle(retweeter_ids)
-
-    #select your followers
-    num_follow_int = int(args.num_followers)
-    print "selecting " + args.num_followers + " followers of " + args.handle + "..."
-    followers = set()
-    for id in retweeter_ids:
-        followers.add(id)
-        if len(followers) >= num_follow_int:
-            break
-
-    # change the type of all ids to string
-    followers = map(lambda x: str(x), followers)
     
-    # write the screen names of the followers to our output file
+    # get active followers (defined as having retweeted the broadcaster ever)
+    retweeter_ids = common.get_retweeters(user_timeline, 1)
+
+    # select your followers randomly and change the id numbers to strings for processing
+    followers = map(lambda x: str(x), np.random.choice(retweeter_ids, min(int(args.num_followers), len(retweeter_ids)), replace=False))
+    
+    # turn the id numbers (as strings) into usernames
+    screen_names = common.get_usernames(followers, 1)
+
+    # write the usernames of the followers to our output file
     with open(args.followers_file, 'wb') as output:
-        print "Finding id of followers..."
-        for i in tqdm(range((len(followers) - 1) / 100 + 1)):
-            while True:
-                try:
-                    time.sleep(1)
-                    # look up up to 100 users in one request (don't hit that rate limit!)
-                    looking_up = ",".join(followers[i * 100 : min((i+1) * 100, len(followers))])
-                    result = twitter.lookup_user(user_id=looking_up)
-                    break
-                except TwythonAuthError as e:
-                    print "TwythonAuthError..."
-                    continue
-                except TwythonRateLimitError as e:
-                    print "TwythonRateLimitError... Waiting for 15 minutes"
-                    for timer in tqdm(range(15*60)):
-                        time.sleep(1)
-                    twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
-                    continue
-            
-            screen_names = map(lambda x: x['screen_name'], result)
-            for name in screen_names:
-                output.write(name+"\n")
+        for name in screen_names:
+            output.write(name+"\n")
     print "Done!"
 
 
-# python get_followers.py --handle <handle> --follower_file PATH/TO/<handle>.txt --start_date YYYY-MM-DD --end_date YYYY-MM-DD
+# python get_followers.py --handle <handle> --num_followers <how many followers you desire> --followers_file PATH/TO/followers/<handle>.txt --start_date YYYY-MM-DD --end_date YYYY-MM-DD
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Specify arguments')
     parser.add_argument('--handle', help='the twitter handle', required=True)
